f7187da27acb1b2fc3bf28355e61a56f25d63e63
fabien lenoir <flenoir@gmail.com>
2019-01-09 11:06:30 +0100

    added user input to select level and refactoring

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
+print("Hi, welcome in Mac Gyver's Maze")
+level_choice = input("Please select a level? 1 or 2 ? ")
+print("Ok, let's go for level " + level_choice)
 
 
-with open("maze_map.py","r") as map:
-    map_array = []
-    for line in map:
-        line_array = []
-        for el in line.strip():
-            print(el)
-            line_array.append(el)
-        map_array.append(line_array)
 
-# print(map_array)
-for item in map_array:
-    print(item)
+class Level:
+    #create level
+    def create_maze(self, file):
+        self.file = file
+        with open(self.file,"r") as map:
+            map_array = []
+            for line in map:
+                line_array = []
+                for el in line.strip():                    
+                    line_array.append(el)
+                map_array.append(line_array)
+            return map_array
+
+    # show level
+    def show_maze(self, result):
+        self.result = result
+        for item in self.result:
+            print(item)
+
+
+if int(level_choice) == 1:
+    #instanciation of level
+    new_level = Level()
+    # level creation
+    res = new_level.create_maze("maze_map.py")
+    # level display
+    new_level.show_maze(res)
+else:
+    print("This level is not available yet")
