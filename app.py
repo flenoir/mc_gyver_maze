9a5aedd2c934f6fc8e850f257cf4ee4a923f3ee6
fabien lenoir <flenoir@gmail.com>
2019-01-09 16:21:01 +0100

    add log of mac gyver position and update maze after mac gyver's move

--- app.py
+++ app.py
@@ -15,13 +15,19 @@ class Level:
         
            
     # show maze
-    def show_maze(self, char1_pos, char1_symbol, char2_pos, char2_symbol):        
+    def show_maze(self, char1_pos, char1_symbol, char2_pos, char2_symbol, position_log):        
         self.char1_pos = char1_pos
         self.char2_pos = char2_pos
         self.char1_symbol = char1_symbol
         self.char2_symbol = char2_symbol
+        # update of mac gyver's position
         self.MAP_ARRAY[char1_pos[0]][char1_pos[1]] = char1_symbol
         self.MAP_ARRAY[char2_pos[0]][char2_pos[1]] = char2_symbol
+        # update of mac gyver's previous position
+        self.position_log = position_log
+        if len(position_log) > 1:
+            print(position_log[-2])
+            self.MAP_ARRAY[position_log[-2][0]][position_log[-2][1]] = '0'
         for item in self.MAP_ARRAY:
             print(item)
 
@@ -35,6 +41,7 @@ class Character:
         self.symbol = symbol
         self.movable = movable
         self.bag_content = bag_content
+        
 
     # move character
     def move(self, position):
@@ -51,6 +58,8 @@ print("Ok, let's go for level " + level_choice)
 
 if int(level_choice) == 1:
 
+    mac_gyver_position_log  = []
+
     #instanciation of level
     new_level = Level("maze_map.py")
     
@@ -62,13 +71,19 @@ if int(level_choice) == 1:
 
     #start game loop
     while end_game != 1:
+        # log mac gyver position
+        mac_gyver_position_log.append(mac_gyver.position)
+        print(mac_gyver_position_log)
+
         # level display
-        new_level.show_maze(mac_gyver.position, mac_gyver.symbol,guardian.position,guardian.symbol)
-            
+        new_level.show_maze(mac_gyver.position, mac_gyver.symbol,guardian.position,guardian.symbol, mac_gyver_position_log)
+          
         select_move = input("please select a new position (one number for line and one for position) for example 12 : ")
         
         # change of mac_gyver position regarding new input
         mac_gyver.position = (int(select_move[0:1]),int(select_move[1:2]))
+
+        print(mac_gyver_position_log)
         
 
 
