1a7ee4e417206868c4ebaa036f26a04f4f63018f
flenoir <flenoir@gmail.com>
2019-01-13 12:18:22 +0100

    refactorisation of show_maze method

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ class Level:
     def __init__(self, file):
         self.file = file
         self.array = []
-        with open(self.file,"r") as map:            
+        with open(self.file, "r") as map:            
             for line in map:
                 line_array = []
                 for el in line.strip():                    
@@ -12,15 +12,14 @@ class Level:
                 self.array.append(line_array)
 
     # show maze
-    def show_maze(self, char1_pos, char1_symbol, char2_pos, char2_symbol):        
+    def show_maze(self, char1, char2):        
         # update of mac gyver's position
-        self.array[char1_pos[0]][char1_pos[1]] = char1_symbol
-        self.array[char2_pos[0]][char2_pos[1]] = char2_symbol
+        self.array[char1.position[0]][char1.position[1]] = char1.symbol
+        self.array[char2.position[0]][char2.position[1]] = char2.symbol
         # parse array and print maze level
         for item in self.array:
             print(item)
         return 0
-
 
 
 class Character:
@@ -32,9 +31,8 @@ class Character:
         self.symbol = symbol
         self.movable = movable
         self.bag_content = bag_content
-        self.position_log =[position]
+        self.position_log = [position]
         
-
     # move character
     def move(self, new_position, maze):
         print("character position is ", self.position)
@@ -49,9 +47,7 @@ class Character:
             print("You cannot move into walls")
             print("after walls ", self.position)
             return self.position
-           # why i don't get display in print ?
-
-
+            # why i don't get display in print ?
 
 
 #### GAME START ####
@@ -63,38 +59,34 @@ print("Ok, let's go for level " + level_choice)
 
 if int(level_choice) == 1:
 
-    #instanciation of level
+    # instanciation of level
     new_level = Level("maze_map.py")
-    
+
     # instanciation of characters
     mac_gyver = Character("Mac Gyver", (0, 1), "M", True, 0)
     guardian = Character("The Guardian", (2, 14), "G", False, 0)
 
-
-
     end_game = 0
 
-    #start game loop
+    # start game loop
     while end_game == 0:
 
         # level display
-        new_level.show_maze(mac_gyver.position, mac_gyver.symbol,guardian.position,guardian.symbol)
+        new_level.show_maze(mac_gyver, guardian)
         
-        select_move = input("please select a new position (one number for line and one for position) for example 12 : ")
+        select_move = input("please select a new position (one number for line, one for position) for example 1,2 : ")
         
         tuplized_move = tuple(int(x) for x in select_move.split(","))
         print("tuple is ", tuplized_move)
 
         # change of mac_gyver position regarding new input
         movment = mac_gyver.move(tuplized_move, new_level.array)
-        print("les movments  actual et previous sont "+ str(movment[0]) + " et " + str(movment[1]))
+        print("les movments  actual et previous sont " + str(movment[0]) + " et " + str(movment[1]))
         # update new current position
         mac_gyver.position = movment[0]
         # update previous position to replace trace by "0"        
         new_level.array[movment[1][0]][movment[1][1]] = "0"
-    
 
 
-        
 else:
     print("This level is not available yet")
