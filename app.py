b158e0a03b28d338e2dbbd25b3b1317b9c4f8890
flenoir <flenoir@gmail.com>
2019-01-22 07:48:46 +0100

    code cleanup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from classes import Level, Character, Game, Player
+from classes import Game, Player
 import constants
 import pygame
 from pygame.locals import *
@@ -8,7 +8,7 @@ jeu = Game(constants.maze1, constants.wall)
 
 mc = Player("Mac Gyver", (0, 1), "images/MacGyver.png", jeu, True, 0)
 g = Player("The Guardian", (2, 14), "images/Gardien.png", jeu, False, 0)
-# jeu.show_maze(mc, g)
+
 
 continuer = 1
 
@@ -17,55 +17,6 @@ while continuer:
         if event.type == QUIT:
             continuer = 0
         else:
-            # jeu.show_maze(mc.position, mc.symbol, mc.position_log)
             jeu.show_maze(mc, g)
             mc.move(event, mc, g)
-        
-            
-    
-
-# if __name__ == "__main__":
-
-#     #### GAME START ####
-
-#     print("Hi, welcome in Mac Gyver's Maze")
-#     level_choice = input("Please select a level? 1 or 2 ? ")
-#     print("Ok, let's go for level " + level_choice)
-
-
-#     if int(level_choice) == 1:
-
-#         # instanciation of level
-#         new_level = Level("maze_map.py")
-
-#         # instanciation of characters
-#         mac_gyver = Character("Mac Gyver", (0, 1), "M", new_level, True, 0)
-#         guardian = Character("The Guardian", (2, 14), "G", new_level, False, 0)
-
-#         end_game = 0
-
-#         # start game loop
-#         while end_game == 0:
-
-#             # level display
-#             new_level.show_maze(mac_gyver, guardian)
-            
-#             select_move = input("please select a new position (one number for line, one for position) for example 1,2 : ")
-            
-#             tuplized_move = tuple(int(x) for x in select_move.split(","))
-#             print("tuple is ", tuplized_move)
-
-#             # change of mac_gyver position regarding new input
-#             movment = mac_gyver.move(tuplized_move)
-#             print("les movments  actual et previous sont " + str(movment[0]) + " et " + str(movment[1]))
-#             # update new current position
-#             mac_gyver.position = movment[0]
-#             # update previous position to replace trace by "0"        
-#             new_level.array[movment[1][0]][movment[1][1]] = "0"
-
-
-#     else:
-#         print("This level is not available yet")
-
-
-# main()
+   
--- classes.py
+++ classes.py
@@ -3,91 +3,8 @@ import pygame
 from pygame.locals import *
 
 
-class Level:
-
-    # create level
-    def __init__(self, file):
-        self.file = file
-        self.array = []
-        self.items = 3
-        with open(self.file, "r") as maze_map:            
-            for line in maze_map:
-                line_array = []
-                for el in line.strip():                    
-                    line_array.append(el)
-                self.array.append(line_array)
-
-        #  display randomly items
-        while self.items != 0:
-            val1, val2 = randint(0, 14), randint(0, 14)
-
-            if self.array[val1][val2] == "0":
-                self.array[val1][val2] = "I"
-                self.items -= 1
-            # else:
-            #     val1, val2 = randint(0, 14), randint(0, 14)
-
-    # show maze
-    def show_maze(self, char1, char2):        
-        # update of mac gyver's position
-        self.array[char1.position[0]][char1.position[1]] = char1.symbol
-        self.array[char2.position[0]][char2.position[1]] = char2.symbol
-        # parse array and print maze level
-        for item in self.array:
-            print(item)
-        return 0
-
-
-class Character:
-
-    # create character
-    def __init__(self, name, position, symbol, maze_level, movable, bag_content):
-        self.name = name
-        self.position = position
-        self.symbol = symbol
-        self.maze_level = maze_level
-        self.movable = movable
-        self.bag_content = bag_content
-        self.position_log = [position]
-       
-    # move character
-    def move(self, new_position):
-        print("character position is ", self.position)
-        # print(maze)
-        if self.maze_level.array[new_position[0]][new_position[1]] == "X":
-            print("You cannot move into walls")
-            print("after walls ", self.position)
-            return self.position
-        elif self.maze_level.array[new_position[0]][new_position[1]] == "0":
-            self.position = new_position
-            print("character new position is ", self.position)
-            self.position_log.append(self.position)
-            print("position log array", self.position_log)
-            return self.position, self.position_log[-2]
-        elif self.maze_level.array[new_position[0]][new_position[1]] == "I":
-            self.bag_content += 1
-            self.position = new_position
-            print("character new position is after picking item ", self.position)
-            self.position_log.append(self.position)
-            print("position log array", self.position_log)
-            return self.position, self.position_log[-2]
-        elif self.maze_level.array[new_position[0]][new_position[1]] == "G":
-            self.position = new_position
-            self.position_log.append(self.position)
-            print("Mac Gyver's bag contains {} items".format(self.bag_content))
-            if self.bag_content != 3:
-                print("Mac Gyver looses")
-                return self.position, self.position_log[-2]
-            else:
-                print("mac Gyvers won")
-                return self.position, self.position_log[-2]
-        else:
-            print("other movment")
-
-
 class Game:
     def __init__(self, maze, wall):
-        # print(maze)
         self.width = maze['x_size']
         self.height = maze['y_size']
         self.background = maze['background']
@@ -110,6 +27,7 @@ class Game:
 
         pygame.init()
         self.area = pygame.display.set_mode((self.width, self.height))
+
         # load background
         background = pygame.image.load(self.background).convert()
         self.area.blit(background, (0, 0))
@@ -122,10 +40,9 @@ class Game:
                 # print(i)
                 if item[i][0] == "X":
                     self.area.blit(wall_block, (int(item[i][2])*45, int(item[i][1])*45))
-                                
-        # return 0
 
-        # # first display of characters position
+
+        # # first display of characters position (to be refactored)
         
         macgyver = pygame.image.load(char1.symbol).convert()
         guardian = pygame.image.load(char2.symbol).convert()
@@ -138,7 +55,6 @@ class Game:
         self.area.blit(macgyver, macgyver_position)
         self.area.blit(guardian, guardian_position)
 
-    
         pygame.display.flip()
 
 
